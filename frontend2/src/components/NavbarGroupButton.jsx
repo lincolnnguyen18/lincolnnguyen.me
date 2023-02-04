6e43359de1569c26faa630c971511e3b2051f82a
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-02-04 01:51:33 -0600

    Add language selector, NavbarGroupDivider

--- frontend2/src/components/NavbarGroupButton.jsx
+++ frontend2/src/components/NavbarGroupButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export function NavbarGroupButton ({ children, twStyle, stopPropagation = false, onClick, disabled = false }) {
+export function NavbarGroupButton ({ children, twStyle, outerTwStyle, stopPropagation = false, onClick, disabled = false }) {
   function handleClick (e) {
     if (disabled || stopPropagation) {
       e.stopPropagation();
@@ -11,12 +11,11 @@ export function NavbarGroupButton ({ children, twStyle, stopPropagation = false,
 
   return (
     <>
-      <button className={twMerge('h-12 px-2 bg-black bg-opacity-50 w-48 cursor-pointer hover:bg-opacity-60 active:bg-opacity-75 active:transition-all active:duration-200', twStyle, disabled && 'hover:bg-opacity-50 cursor-not-allowed active:bg-opacity-50')} type="button" onClick={handleClick}>
-        <div className={twMerge(disabled && 'opacity-50', 'flex items-center gap-2')}>
+      <button className={twMerge('h-12 px-2 bg-black bg-opacity-50 w-48 cursor-pointer hover:bg-opacity-60 active:bg-opacity-75 active:transition-all active:duration-200 first:rounded-t-lg last:rounded-b-lg', outerTwStyle, disabled && 'hover:bg-opacity-50 cursor-not-allowed active:bg-opacity-50')} type="button" onClick={handleClick}>
+        <div className={twMerge(disabled && 'opacity-50', 'flex items-center gap-2', twStyle)}>
           {children}
         </div>
       </button>
-      {(!twStyle || (twStyle && !twStyle.includes('rounded-b-lg'))) && <div className="h-[1px] bg-black bg-opacity-40 w-48" />}
     </>
   );
 }
---
+++ frontend2/src/components/NavbarGroupDivider.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function NavbarGroupDivider () {
+  return (
+    <div className="h-[1px] bg-black bg-opacity-30" />
+  );
+}
