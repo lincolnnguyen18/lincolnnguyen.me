f7fca9ab31f705d25834b15253b5bddf2a34bac3
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-02-05 16:43:09 -0600

    Fix BackButton propagation bug

--- frontend2/src/components/BackButton.jsx
+++ frontend2/src/components/BackButton.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export function BackButton ({ twStyle, linkPath, text = 'Back', ...rest }) {
+export function BackButton ({ twStyle, linkPath, text = 'Back', onClick, stopPropagation = true, ...rest }) {
+  function handleClick (e) {
+    if (!linkPath) {
+      if (stopPropagation) e.stopPropagation();
+      onClick && onClick();
+    }
+  }
+
   const content = (
     <button
       className={`flex items-center gap-1 cursor-pointer active:opacity-50 transition-opacity duration-75 ${twStyle}`}
       type="button"
+      onClick={handleClick}
       {...rest}
     >
       <span className="icon-back text-2xl " />
