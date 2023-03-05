cb0d813f6cf80b1496a9ec10ba1c17703924841a
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-04 20:22:49 -0600

    Fix rounded input corners ios

--- frontend/src/components/TextField.js
+++ frontend/src/components/TextField.js
@@ -3,6 +3,6 @@ import { twMerge } from 'tailwind-merge';
 
 export function TextField ({ twStyle, dir = 'single', ...rest }) {
   return (
-    <input className={twMerge('text-white bg-black bg-opacity-50 h-11 px-3 w-full placeholder:text-gray-input', dir === 'single' && 'rounded-lg', dir === 'vert' && 'first:rounded-t-lg last:rounded-b-lg', twStyle)} {...rest} />
+    <input className={twMerge('text-white bg-black bg-opacity-50 h-11 px-3 w-full placeholder:text-gray-input', dir === 'single' && 'rounded-lg', dir === 'vert' && 'rounded-none first:rounded-t-lg last:rounded-b-lg', twStyle)} {...rest} />
   );
 }
