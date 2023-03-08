eb341c866ed4b33f67e7a0605e37ebe9252c855b
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-07 22:51:13 -0600

    Fit chevron arrow inside dropdown clickable area

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -3,11 +3,11 @@ import { twMerge } from 'tailwind-merge';
 
 export function Dropdown ({ children, twStyle, outerTwStyle, ...rest }) {
   return (
-    <div className={twMerge('flex items-center text-gray-input', outerTwStyle)}>
-      <select className={twMerge('appearance-none py-1 px-2 bg-none bg-transparent cursor-pointer w-[120px] overflow-hidden truncate', twStyle)} {...rest}>
+    <div className={twMerge('flex items-center text-gray-input relative', outerTwStyle)}>
+      <select className={twMerge('appearance-none py-1 pl-2 pr-7 bg-none bg-transparent cursor-pointer w-[140px] overflow-hidden truncate', twStyle)} {...rest}>
         {children}
       </select>
-      <span className="icon-chevron-right text-2xl" />
+      <span className="icon-chevron-right text-2xl absolute right-1 top-0 bottom-0 m-auto pointer-events-none" />
     </div>
   );
 }
