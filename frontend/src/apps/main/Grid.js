f9732187f7ae030bbc957e15b1a344c28438d839
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-17 00:02:46 -0500

    Temporarily disable horizontal grid home screen

--- frontend/src/apps/main/Grid.js
+++ frontend/src/apps/main/Grid.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { commonSelector } from '../../slices/commonSlice.js';
 import { OverflowContainer } from '../../components/OverflowContainer';
 
 export function Grid ({ children }) {
-  const { browser } = useSelector(commonSelector);
+  // const { browser } = useSelector(commonSelector);
 
-  if (!browser?.os.startsWith('Android') && !browser?.os.startsWith('iOS')) {
-    return (
-      <OverflowContainer className="p-0">
-        <div className="max-w-screen-sm w-full mx-auto grid grid-cols-4 grid-flow-row gap-4 place-items-center px-2 pt-12 pb-4">
-          {children}
-        </div>
-      </OverflowContainer>
-    );
-  } else {
-    return (
-      <div className="fixed left-0 right-0 top-0 bottom-0 overflow-x-scroll">
-        <div className="w-fit px-4">
-          <div className="grid grid-rows-4 grid-flow-col gap-4 place-items-center pt-12 pb-4">
-            {children}
-          </div>
-        </div>
+  // if (!browser?.os.startsWith('Android') && !browser?.os.startsWith('iOS')) {
+  //   return (
+  //     <OverflowContainer className="p-0">
+  //       <div className="max-w-screen-sm w-full mx-auto grid grid-cols-4 grid-flow-row gap-4 place-items-center px-2 pt-12 pb-4">
+  //         {children}
+  //       </div>
+  //     </OverflowContainer>
+  //   );
+  // } else {
+  //   return (
+  //     <div className="fixed left-0 right-0 top-0 bottom-0 overflow-x-scroll">
+  //       <div className="w-fit px-4">
+  //         <div className="grid grid-rows-4 grid-flow-col gap-4 place-items-center pt-12 pb-4">
+  //           {children}
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <OverflowContainer className="p-0">
+      <div className="max-w-screen-sm w-full mx-auto grid grid-cols-4 grid-flow-row gap-4 place-items-center px-2 pt-12 pb-4">
+        {children}
       </div>
-    );
-  }
+    </OverflowContainer>
+  );
 }
