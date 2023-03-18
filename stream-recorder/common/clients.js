b09e63007ec9abe3f2ef2c38a244cb3adb93bc80
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-18 11:51:18 -0500

    Add stream recorder

---
+++ stream-recorder/common/clients.js
@@ -0,0 +1,12 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { environment } from './environment.js';
+
+const s3Client = new S3Client({
+  region: environment.AWS_REGION,
+  credentials: {
+    accessKeyId: environment.AWS_ACCESS_KEY_ID,
+    secretAccessKey: environment.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+export { s3Client };
