b77169db02db71b8c95d01918223c461166c61f8
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-19 23:11:30 -0500

    Time table parser works for one column in tv tokyo

--- timetable-parser/src/index.ts
+++ timetable-parser/src/index.ts
@@ -1,5 +1,5 @@
+import { getBoundingClientRect, getElementAtPoint, setElementBackgroundColor, textIsPresent, wait } from 'utils';
 import { remote } from 'webdriverio';
-import { getBottomRightCorner, setElementBackgroundColor, textIsPresent } from 'utils';
 
 async function googleSearch () {
   const browser = await remote({
@@ -14,20 +14,56 @@ async function googleSearch () {
   await browser.waitUntil(() => browser.execute(textIsPresent, '5:25'), { timeout: 10000 });
   // await browser.execute(() => alert('Page loaded!'));
 
-  const point = { x: 250, y: 350 };
-  const { bottomRight, height } = await browser.execute(getBottomRightCorner, point);
-  console.log('Bottom right corner:', bottomRight);
-  console.log('Height:', height);
-  await browser.execute(setElementBackgroundColor, bottomRight);
-  // console.log('Bottom right corner:', bottomRightCorner);
+  await browser.execute(() => {
+    const element = document.querySelector('#timetable_weekly_body > tr:nth-child(1446) > td:nth-child(1)');
+    if (!element) return;
+    element.setAttribute('style', 'height: 1000px;');
+  });
 
-  // // const searchInput = await browser.$('[name="q"]');
-  // // await searchInput.setValue('webdriverio');
-  // // await browser.keys(Key.Enter);
-  // // await browser.pause(2000); // Wait for the search results to load
-  // // const searchResults = await browser.$$('#search .g');
-  // // console.log(`Number of search results: ${searchResults.length}`);
-  // // await browser.deleteSession();
+  /*
+
+    Pseudocode
+
+    Given starting point, get element at point
+    
+    While element is not null
+      Set element background color to red
+      Get element bounding rectangle
+      Scroll element out of view (scroll to bottom of element)
+      Get new element at point
+
+  */
+
+  await browser.execute(() => window.scrollTo(0, 222));
+  await wait(1000);
+  // await browser.execute(() => alert('Starting'));
+  const startingPoint = { x: 250, y: 60 };
+  const cells = [];
+  let element = await browser.execute(getElementAtPoint, startingPoint);
+  // append visible text of element to cells
+  let text = await browser.execute(element => element.textContent, element);
+  cells.push(text);
+
+  while (element) {
+    await browser.execute(setElementBackgroundColor, element);
+    const rect = await browser.execute(getBoundingClientRect, element);
+    await browser.execute(rect => window.scrollBy(0, rect.height), rect);
+    element = await browser.execute(getElementAtPoint, startingPoint);
+    text = await browser.execute(element => element.textContent, element);
+    if (text === '') break;
+    if (cells[cells.length - 1] !== text) {
+      cells.push(text);
+    }
+    await browser.execute((cells) => console.log('cells:', cells), cells);
+  }
+
+  // const searchInput = await browser.$('[name="q"]');
+  // await searchInput.setValue('webdriverio');
+  // await browser.keys(Key.Enter);
+  // await browser.pause(2000); // Wait for the search results to load
+  // const searchResults = await browser.$$('#search .g');
+  // console.log(`Number of search results: ${searchResults.length}`);
+  // await browser.deleteSession();
 }
 
 googleSearch();
--- timetable-parser/src/utils.ts
+++ timetable-parser/src/utils.ts
@@ -5,17 +5,56 @@ interface Point {
 
 function getBottomRightCorner (point : Point) : { bottomRight: Point, height: number } | null {
   const element = document.elementFromPoint(point.x, point.y);
-  if (!element) return null;
+  if (!element) {
+    console.log('getBottomRightCorner; No element found at point:', point);
+    return null;
+  }
   console.log('Element text:', element.textContent);
   const rect = element.getBoundingClientRect();
   const bottomRight = { x: rect.x + rect.width, y: rect.y + rect.height };
   const height = rect.height;
+  
+  bottomRight.x -= 10;
+  bottomRight.y -= 10;
+
+  console.log('Bottom right corner:', bottomRight);
+  console.log('Height:', height);
+
+  // insert 10px by 10px blue circle at bottomRight position, use absolute positioning
+  const circle = document.createElement('div');
+  // set id to circle
+  circle.setAttribute('style', 'position: absolute; width: 10px; height: 10px; border-radius: 50%; background-color: white; z-index: 999999; top: ' + bottomRight.y + 'px; left: ' + bottomRight.x + 'px;');
+  document.body.appendChild(circle);
+
   return { bottomRight, height };
 }
 
-function setElementBackgroundColor (point : Point) : void {
+function getHeight (point : Point) : number | null {
   const element = document.elementFromPoint(point.x, point.y);
-  if (!element) return;
+  if (!element) {
+    console.log('getHeight; No element found at point:', point);
+    return null;
+  }
+  const rect = element.getBoundingClientRect();
+  const height = rect.height;
+  console.log('Height:', height);
+  return height;
+}
+
+// function setElementBackgroundColor (point : Point) : void {
+//   const element = document.elementFromPoint(point.x, point.y);
+//   if (!element) {
+//     console.log('setElementBackgroundColor; No element found at point:', point);
+//     return;
+//   }
+//   element.setAttribute('style', 'background-color: red;');
+//   const children = element.querySelectorAll('*');
+//   children.forEach((child) => {
+//     child.setAttribute('style', 'color: white;');
+//   });
+// }
+
+function setElementBackgroundColor (element: Element) : void {
   element.setAttribute('style', 'background-color: red;');
 }
 
@@ -27,4 +66,22 @@ function wait (ms : number) : Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export { getBottomRightCorner, textIsPresent, setElementBackgroundColor, wait };
+function scrollIntoView (point : Point) : void {
+  const element = document.elementFromPoint(point.x, point.y);
+  if (!element) return;
+  element.scrollIntoView();
+}
+
+function getElementAtPoint (point : Point) : Element | null {
+  const element = document.elementFromPoint(point.x, point.y);
+  console.log('getElementAtPoint; element:', element);
+  return element;
+}
+
+function getBoundingClientRect (element: Element) : DOMRect | null {
+  const rect = element.getBoundingClientRect();
+  console.log('getBoundingClientRect; rect:', rect);
+  return rect;
+}
+
+export { getBottomRightCorner, textIsPresent, setElementBackgroundColor, wait, getHeight, scrollIntoView, getElementAtPoint, getBoundingClientRect };
