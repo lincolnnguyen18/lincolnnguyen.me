ba03f9f2b36b3db2b26340d321d9721cca6c421c
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-21 17:34:06 -0500

    Scroll apps when on larger screen

--- frontend2/src/apps/home/Apps.tsx
+++ frontend2/src/apps/home/Apps.tsx
@@ -27,42 +27,56 @@ export default function Apps ({ apps }: AppsProps) {
   }
 
   function onScrollChange (props: onScrollChangeProps) {
-    const { isScrolling, scrollLeft } = props;
-    // if scrollLeft is not a multiple of screenWidth, scrollTo the nearest multiple
-    if (!isScrolling && scrollLeft % screenWidth !== 0) {
-      root.scrollTo({
-        left: Math.round(scrollLeft / screenWidth) * screenWidth,
-        behavior: 'smooth',
-      });
-      return;
-    }
+    const { scrollLeft } = props;
     const currentPage = Math.floor((scrollLeft + screenWidth / 2) / screenWidth);
     setCurrentPage(currentPage);
   }
 
-  return (
-    <React.Fragment>
-      <div className='flex flex-row'>
-        {appGroups.map((appGroup, i) => (
-          <SnapScrollContainer key={i}>
-            <Screen>
-              <div className='grid grid-cols-4 grid-flow-row gap-4 place-items-center px-6 pt-2 pb-4'>
-                {appGroup}
-              </div>
-            </Screen>
-          </SnapScrollContainer>
-        ))}
-      </div>
-      <div className='flex flex-row gap-3 justify-center fixed w-screen pointer-events-none mt-8'>
-        {appGroups.map((_, i) => (
-          <div
-            key={i}
-            className='bg-white w-2 h-2 rounded-full transition-opacity duration-200 mb-11'
-            style={{ opacity: i === currentPage ? 1 : 0.4 }}
-          />
-        ))}
-      </div>
-      <ScrollListener target={root} onScrollChange={onScrollChange} scrollTimeout={100} />
-    </React.Fragment>
-  );
+  React.useEffect(() => {
+    const horizonalScrollClasses = ['snap-x', 'snap-mandatory', 'overflow-x-scroll', 'h-screen'];
+    if (screenWidth <= parseInt(theme.screens.sm)) {
+      root.classList.add(...horizonalScrollClasses);
+    } else {
+      root.classList.remove(...horizonalScrollClasses);
+    }
+    return () => {
+      root.classList.remove(...horizonalScrollClasses);
+    };
+  }, [screenWidth]);
+
+  if (screenWidth <= parseInt(theme.screens.sm)) {
+    return (
+      <React.Fragment>
+        <div className='flex flex-row'>
+          {appGroups.map((appGroup, i) => (
+            <SnapScrollContainer key={i}>
+              <Screen>
+                <div className='grid grid-cols-4 grid-flow-row gap-4 place-items-center px-6 pt-2 pb-4'>
+                  {appGroup}
+                </div>
+              </Screen>
+            </SnapScrollContainer>
+          ))}
+        </div>
+        <div className='flex flex-row gap-3 justify-center fixed w-screen pointer-events-none mt-8'>
+          {appGroups.map((_, i) => (
+            <div
+              key={i}
+              className='bg-white w-2 h-2 rounded-full transition-opacity duration-200 mb-11'
+              style={{ opacity: i === currentPage ? 1 : 0.4 }}
+            />
+          ))}
+        </div>
+        <ScrollListener target={root} onScrollChange={onScrollChange} scrollTimeout={100} />
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <Screen>
+        <div className='grid grid-cols-4 grid-flow-row gap-4 place-items-center px-6 pt-2 pb-4'>
+          {apps}
+        </div>
+      </Screen>
+    );
+  }
 }
--- frontend2/src/apps/home/HomeScreen.tsx
+++ frontend2/src/apps/home/HomeScreen.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import Nav from 'components/Nav';
 import Apps from 'apps/home/Apps';
 import AppIcon from 'apps/home/AppIcon';
-import { useSelector } from 'react-redux';
-import { commonSelector } from 'slices/commonSlice';
 
 export default function HomeScreen () {
-  const { root } = useSelector(commonSelector);
   const [apps, setApps] = React.useState<React.ReactNode[]>([]);
 
   React.useEffect(() => {
-    // setup horizontal snap scrolling
-    const classes = ['snap-x', 'snap-mandatory', 'overflow-x-scroll', 'h-screen'];
-    root.classList.add(...classes);
-
-    // set apps
     setApps([
       ...Array.from({ length: 30 }).map((_, i) => (
         <AppIcon abbreviation='AB' name='Aiden B' className='bg-red-400' key={i} />
@@ -26,10 +18,6 @@ export default function HomeScreen () {
         <AppIcon abbreviation='AB' name='Aiden B' className='bg-green-400' key={i} />
       )),
     ]);
-
-    return () => {
-      root.classList.remove(...classes);
-    };
   }, []);
 
   return (
