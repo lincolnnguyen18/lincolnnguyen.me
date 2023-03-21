d8043359016a328326637c87e42bca35a7ef438c
Lincoln Nguyen <61459043+lincolnnguyen18@users.noreply.github.com>
2023-03-21 03:53:08 -0500

    Parsing works for tvtokyo timetable

--- timetable-parser/src/index.ts
+++ timetable-parser/src/index.ts
@@ -1,4 +1,4 @@
-import { getCellsInColumn, setAttributeOfElement, textIsPresent } from 'utils';
+import { getCellsInTable, setAttributeOfElement, setAttributeOfElements, textIsPresent, wait } from 'utils';
 import { remote } from 'webdriverio';
 
 async function googleSearch () {
@@ -14,13 +14,18 @@ async function googleSearch () {
   await browser.waitUntil(() => browser.execute(textIsPresent, '5:25'), { timeout: 10000 });
 
   const toResizeQuerySelector = '#timetable_weekly_body > tr:nth-child(1446) > td:nth-child(1)';
-  await browser.execute(setAttributeOfElement, toResizeQuerySelector, 'style', 'height: 1000px;');
+  await browser.execute(setAttributeOfElement, toResizeQuerySelector, 'style', 'height: 7000px;');
 
-  const startingScrollPosition = 222;
-  const startingPoint = { x: 250, y: 60 };
-  const cells = await getCellsInColumn(browser, startingScrollPosition, startingPoint);
-  
-  await browser.execute((cells) => console.log('cells:', cells), cells);
+  const toAddPaddingQuerySelector = '.tbcms_timetable--state_weekly .tbcms_timetable__inner';
+  await browser.execute(setAttributeOfElements, toAddPaddingQuerySelector, 'style', 'padding-right: 70px;');
+  await wait(1000);
+
+  const firstCell = { x: 247, y: 165 };
+  const columnWidth = 220;
+  const numColumns = 7;
+  const scrollAmount = 70;
+  const columns = await getCellsInTable(browser, firstCell, columnWidth, numColumns, scrollAmount);
+  await browser.execute((columns) => console.log('columns:', columns), columns);
 }
 
 googleSearch();
--- timetable-parser/src/utils.ts
+++ timetable-parser/src/utils.ts
@@ -3,68 +3,145 @@ interface Point {
   y: number;
 }
 
-function setElementBackgroundColor (element: Element) : void {
-  element.setAttribute('style', 'background-color: red;');
-}
-
-function textIsPresent (text : string) : boolean {
+function textIsPresent (text: string): boolean {
   return document.querySelector('body')?.textContent.includes(text);
 }
 
-function wait (ms : number) : Promise<void> {
+function wait (ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function getElementInfo (element : Element) : { text : string, rect : DOMRect } {
+function getElementInfo (element: Element): { text: string; rect: DOMRect } {
   return {
     text: element.textContent.trim(),
     rect: element.getBoundingClientRect(),
   };
 }
 
-function logInBrowser (message : any) : void {
+function logInBrowser (message): void {
   console.log(message);
 }
 
-function getElementAtPoint (point : Point) : Element | null {
+function getElementAtPoint (point: Point): Element | null {
   const element = document.elementFromPoint(point.x, point.y);
-  console.log('getElementAtPoint; element:', element);
+  // console.log('getElementAtPoint; element:', element);
   return element;
 }
 
-function getBoundingClientRect (element: Element) : DOMRect | null {
+function getBoundingClientRect (element: Element): DOMRect | null {
   const rect = element.getBoundingClientRect();
-  console.log('getBoundingClientRect; rect:', rect);
+  // console.log('getBoundingClientRect; rect:', rect);
   return rect;
 }
 
-function scrollTo (scrollPosition : number) : void {
+function scrollTo (scrollPosition: number): void {
   window.scrollTo(0, scrollPosition);
 }
 
-async function getCellsInColumn (browser, startingScrollPosition: number, startingPoint : Point) : Promise<string[]> {
+async function getCellsInColumn (
+  browser: WebdriverIO.Browser,
+  firstCell: Point,
+  scrollByAmount: number,
+): Promise<string[]> {
   const cells = [];
-  await browser.execute(scrollTo, startingScrollPosition);
-  await wait(1000);
+  await browser.execute(scrollTo, 0);
 
   do {
-    const element = await browser.execute(getElementAtPoint, startingPoint);
-    const { text, rect } = await browser.execute(getElementInfo, element);
-    if (text === '') {
+    const element = await browser.execute(getElementAtPoint, firstCell);
+    const { text } = await browser.execute(getElementInfo, element);
+    if (text === '' && cells.length > 0) {
       break;
     }
-    cells.push(text);
-    await browser.execute(setElementBackgroundColor, element);
-    await browser.execute(rect => window.scrollBy(0, rect.height), rect);
+    // check if text contains numbers matching time format such as 27:15 or 4:00 in addition to other text
+    let validCell = false;
+    const matchedText = text.match(/\d{1,2}:\d{2}/);
+    if (matchedText?.length > 0 && matchedText[0] !== text) {
+      validCell = true;
+    }
+    if (text !== '' && text !== cells[cells.length - 1] && validCell) {
+      cells.push(text);
+      await browser.execute(
+        setAttributeOfElement,
+        element,
+        'style',
+        'background-color: red;',
+      );
+    }
+    await wait(1);
+    await browser.execute(scrollBy, 0, scrollByAmount);
+    await wait(1);
   } while (true);
-  
+
   return cells;
 }
 
-function setAttributeOfElement (querySelector : string, attribute : string, value : string) : void {
-  const element = document.querySelector(querySelector);
+async function getCellsInTable (
+  browser: WebdriverIO.Browser,
+  firstCell: Point,
+  columnWidth: number,
+  numColumns: number,
+  scrollAmount: number,
+): Promise<string[][]> {
+  const columns = [];
+  for (let i = 0; i < numColumns; i++) {
+    const cells = await getCellsInColumn(
+      browser,
+      { x: firstCell.x + i * columnWidth, y: firstCell.y },
+      scrollAmount,
+    );
+    // console.log('cells:', cells);
+    columns.push(cells);
+    browser.execute(scrollTo, 0);
+
+    await wait(100);
+  }
+  return columns;
+}
+
+function scrollBy (x: number, y: number): void {
+  window.scrollBy(x, y);
+}
+
+/**
+ * @param element - querySelector or Element
+ */
+function setAttributeOfElement (
+  element: string | Element,
+  attribute: string,
+  value: string,
+): void {
+  if (typeof element === 'string') {
+    element = document.querySelector(element);
+  }
   if (!element) return;
   element.setAttribute(attribute, value);
 }
 
-export { textIsPresent, setElementBackgroundColor, wait, getCellsInColumn, logInBrowser, getElementInfo, getElementAtPoint, getBoundingClientRect, setAttributeOfElement };
+function setAttributeOfElements (
+  elements: string | Element[],
+  attribute: string,
+  value: string,
+): void {
+  if (typeof elements === 'string') {
+    const nodeList = document.querySelectorAll(elements);
+    elements = Array.from(nodeList);
+  }
+  if (!elements) return;
+  elements.forEach((element) => {
+    element.setAttribute(attribute, value);
+  });
+}
+
+
+export {
+  textIsPresent,
+  wait,
+  getCellsInColumn,
+  logInBrowser,
+  getElementInfo,
+  getElementAtPoint,
+  getBoundingClientRect,
+  setAttributeOfElement,
+  setAttributeOfElements,
+  getCellsInTable,
+};
